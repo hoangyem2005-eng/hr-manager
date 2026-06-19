e2cf03273774ff86755ac4fb2a79323e85bd7f40
Ngoan <nguyenthibengoan.0223@gmail.com>
2026-06-19 12:47:45 +0700

    cập nhật code login/register,quên mật khẩu khi nhập email thì sẽ gửi link về code ở mở file storage/logs/laravel.log kéo xuống cuối sẽ thấy link tương tự link này :http://127.0.0.1:8000/reset-password/8ccb61298c7ce2ba5f476c153ddbe91bbea9625cf70cfcab6312552446c7295b?email=nguyenthibengoan.0223%40gmail.com . gắn qua tab khác

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController; 
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/login', [AuthController::class, 'showLogin']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/register', [AuthController::class, 'showRegister']);
@@ -17,6 +18,14 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('auth.dashboard');
 })->middleware('auth');
 
+
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('password.request');
+
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
+
+Route::post('/reset-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
